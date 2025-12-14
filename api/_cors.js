fa3b6bc3521b6273270191e5207fc6dc3de8757b
Levin68 <160054762+Levin68@users.noreply.github.com>
2025-12-14 11:55:05 +0700

    Update _cors.js

--- api/_cors.js
+++ api/_cors.js
@@ -1,15 +1,12 @@
 // api/_cors.js
-export function applyCors(req, res) {
-  // kalau mau ketat, ganti "*" jadi domain frontend lo
+module.exports.applyCors = function applyCors(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "https://api-orkut-brown.vercel.app/");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Auth-User");
   res.setHeader("Access-Control-Max-Age", "86400");
-
-  // preflight
   if (req.method === "OPTIONS") {
     res.status(204).end();
-    return true; // stop handler
+    return true;
   }
   return false;
-}
+};
